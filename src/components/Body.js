2ee99d35cd46a0dfbcf68473209e95e03bfd5d76
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-11-13 16:13:59 +0530

    fetching the live data from  swiggy api instaed of mock data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,21 @@
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
 
     const [listOfRestaurant, setListOfRestaurant] = useState(resList);
     const filterTopRestaurants = listOfRestaurant.filter(res => res.info.avgRating >= 4);
+
+    useEffect(() => {
+        const fetchApiData = async () => {
+            const apiData = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.6484063&lng=73.7645644&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+            const apiJson = await apiData.json();
+            // console.log(apiJson);
+            setListOfRestaurant(apiJson.data?.cards[5]?.card.card.gridElements?.infoWithStyle?.restaurants)
+        }
+        fetchApiData();
+    }, [])
 
     return (
         <div className="body">
