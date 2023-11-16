4b9972a80ab988a52cbc4b43c0c2d95e1d3e965a
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-11-17 00:11:44 +0530

    moved routing from page level to component level with children Routing, Outlet and Link

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Body from "./components/Body";
 import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 
 const AppLayout = () => {
     return (
         <div className="app">
             <Header />
-            <Body />
+            <Outlet />
         </div>
     )
 }
@@ -20,15 +20,23 @@ const route = createBrowserRouter([
     {
         path: '/',
         element: <AppLayout />,
-        errorElement: <Error />
-    }, {
-        path: '/about',
-        element: <About />,
-    }, {
-        path: '/contact',
-        element: <Contact />
+        children: [
+            {
+                path: '/',
+                element: <Body />,
+            },
+            {
+                path: '/about',
+                element: <About />,
+            },
+            {
+                path: '/contact',
+                element: <Contact />,
+            }
+        ],
+        errorElement: <Error />,
+    },
+]);
 
-    }
-])
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={route} />)
