e9ad43eb8efaea8dafe68083d1512a2e7086fa5e
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-11-22 10:41:09 +0530

    create a sate variable in class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 const User = (props) => {
     const { name, location, contact } = props;
+    const [count1] = useState(1);
+    const [count2] = useState(2);
+    
     return (
         <div className="user-card">
             <h2>Name : {name}</h2>
             <h3>Location :{location} </h3>
             <h3>Contact : {contact}</h3>
+            <h3>Count1 : {count1}</h3>
+            <h3>Count2 : {count2}</h3>
         </div>
     );
 };
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,16 +2,23 @@ import React from "react";
 
 class UserClass extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            count1: 1,
+            count2: 2,
+        }
     }
     render() {
         const { name, location, contact } = this.props;
+        const { count1, count2 } = this.state;
 
         return (
             <div className="user-card">
                 <h2>Name : {name}</h2>
                 <h3>Location :{location} </h3>
                 <h3>Contact : {contact}</h3>
+                <h3>Count1 : {count1}</h3>
+                <h3>Count2 : {count2}</h3>
             </div>
         );
     }
