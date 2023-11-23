362a807784ef71d8aba8429c997bb695f1f50c76
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-11-23 22:07:44 +0530

    updating a state variable in class component

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
             <h1>About Us 🉐</h1>
             <h3>Hey there this is about us component</h3>
 
-            <User name={'Prakash Joshi'} location={"Pune, India"} contact={"@prakashjoshi"} />
+            {/* <User name={'Prakash Joshi'} location={"Pune, India"} contact={"@prakashjoshi"} /> */}       
             <UserClass name={'Prakash Joshi'} location={"Pune, India"} contact={"@prakashjoshi"} />
         </div>
 
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,21 +4,24 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            count1: 1,
-            count2: 2,
+            count: 0,
         }
     }
     render() {
         const { name, location, contact } = this.props;
-        const { count1, count2 } = this.state;
+        const { count } = this.state;
 
         return (
             <div className="user-card">
+                <button onClick={() => {
+                    this.setState({ count: this.state.count + 1 })
+                }}>
+                    <h1>Count : {count}</h1>
+                </button>
                 <h2>Name : {name}</h2>
                 <h3>Location :{location} </h3>
                 <h3>Contact : {contact}</h3>
-                <h3>Count1 : {count1}</h3>
-                <h3>Count2 : {count2}</h3>
+                {/* <h3>Count2 : {count2}</h3> */}
             </div>
         );
     }
