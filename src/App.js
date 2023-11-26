eb628deaeb54a42f839513fe094bd1a7d914a022
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-11-26 13:26:02 +0530

    create lazy loading and suspense fallback for About component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
 import RestaurantMenu from './components/RestaurantMenu'
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+
+const About = lazy(() => import("./components/About"))
 
 const AppLayout = () => {
     return (
@@ -28,7 +29,8 @@ const route = createBrowserRouter([
             },
             {
                 path: '/about',
-                element: <About />,
+                element: (<Suspense fallback={<h1>Loading....</h1>}><About /></Suspense>
+                ),
             },
             {
                 path: '/contact',
