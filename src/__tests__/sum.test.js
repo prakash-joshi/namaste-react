19949d1ce0231f8d1266eb67993b4cd99f0c88f0
prakash-joshi <prakash.joshi167@hotmail.com>
2024-02-01 00:40:35 +0530

    add a js function sum to add 2 numbers and create a test case for it

---
+++ src/__tests__/sum.test.js
@@ -0,0 +1,8 @@
+import { sum } from "../utils/sum";
+
+test("Sum function should calculate the sum of two numbers.", () => {
+    const result = sum(2, 3);
+
+    //assertion
+    expect(result).toBe(5);
+});
